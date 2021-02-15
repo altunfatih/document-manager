9b1fabba5124bdeeb4ed2ec44fcec4d40f64eca0
fatih <fatih.altun00@hotmail.com>
2021-02-15 18:39:40 +0300

    first commit

---
+++ src/test/java/com/fatih/DocumentManager/DocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.DocumentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
